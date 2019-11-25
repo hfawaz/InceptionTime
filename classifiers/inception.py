d17929518476bb74ac0da47b858254d7b3f3d473
Yao Lu <luyaocns@gmail.com>
2019-11-25 23:51:49 +0800

    modify path

--- classifiers/inception.py
+++ classifiers/inception.py
@@ -10,7 +10,7 @@ from utils.utils import save_test_duration
 
 class Classifier_INCEPTION:
 
-    def __init__(self, output_directory, input_shape, nb_classes, verbose=False, build=True, batch_size=64,
+    def __init__(self, output_directory, input_shape, nb_classes, verbose=True, build=True, batch_size=64,
                  nb_filters=32, use_residual=True, use_bottleneck=True, depth=6, kernel_size=41, nb_epochs=1500):
 
         self.output_directory = output_directory
